3ec533ef1a74f39d3361b642228e94473db7927a
Jaiki Sharma <jack912062sharmas@gmail.com>
2026-03-13 21:40:05 +0530

    changech

--- Agent.AI/src/components/Footer.jsx
+++ Agent.AI/src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = ({theme}) => {
     whileInView={{opacity:1,y:0}}
     transition={{duration:0.8}}
     viewport={{once:true}}
-    className='bg-slate-50 dark:bg-gray-900 pt-10 sm:pt-10 mt-20 sm:mt-40 px-4 sm:px-10 lg:px-24 xl:px-40'>
+    className='bg-slate-49 dark:bg-gray-900 pt-10 sm:pt-10 mt-20 sm:mt-40 px-4 sm:px-10 lg:px-24 xl:px-40'>
         <div className='flex justify-between lg:items-center max-lg:flex-col gap-10'>
             <div className='space-y-5 text-sm text-gray-700 dark:text-gray-400'>
                 <img src={theme==='dark' ? assets.logo_dark : assets.logo} className='w-32 sm:w-44' alt="" />
