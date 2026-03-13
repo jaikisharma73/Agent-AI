461a1ce72a31a5c4122b8c4a51868aa7ebe65c6d
Jaiki Sharma <jack912062sharmas@gmail.com>
2026-03-13 21:59:03 +0530

    dd

--- Agent.AI/src/App.jsx
+++ Agent.AI/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import TrustedBy from './components/TrustedBy'
 import ContactUs from './components/ContactUs'
