e60f3a07597cd8b9f97f1df9c74ee28c066e46a5
Jaiki Sharma <jack912062sharmas@gmail.com>
2026-03-14 16:00:25 +0530

    hight

--- Agent.AI/src/components/ServiceCard.jsx
+++ Agent.AI/src/components/ServiceCard.jsx
@@ -10,7 +10,7 @@ const ServiceCard = ({service , index}) => {
     whileInView={{opacity:1,y:0}}
     transition={{duration:0.8,delay:0.4}}
     viewport={{once:true}}
-    className='relative h-50 overflow-hidden max-w-lg m-2 sm:m-4 rounded-xl border border-gray-200 dark:border-gray-700 shadow=gray-100 dark:shadow-white/10'>
+    className='relative h-auto overflow-hidden max-w-lg m-2 sm:m-4 rounded-xl border border-gray-200 dark:border-gray-700 shadow=gray-100 dark:shadow-white/10'>
         <div className='pointer-events-none blur-2xl rounded-full bg-gradient-to-r from-red-500 to-pink-500 w-[300px] h-[300px] absolute z-0 transition-opacity duration-500 mix-blend-lighten opacity-70' style={{top:position.y - 150 , left: position.x - 150}}/>
             <div className='flex items-center gap-10 p-8 hover:p-7.5 hover:m-0.5 transition-all rounded-[10px] bg-white dark:bg-black z-10 relative'>
                 <div className='bg-gray-100 dark:bg-gray-700 rounded-full'>
